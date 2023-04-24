bddbf55844df0e07dc4192cce367e763c2b3f92c
Willian Santaana <williansantaana@gmail.com>
2023-04-25 00:34:26 +0100

    Created project scope

---
+++ database/migrations/2023_04_24_230204_create_post_tags_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->id('id');
+            $table->foreignId('post_id')->unique()->constrained()->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreignId('tag_id')->unique()->constrained()->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tags');
+    }
+};
